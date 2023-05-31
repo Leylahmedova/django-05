6099ebc3f64c224b6fdf57cd0333458f75ebff99
leyla ahmadova <leylaahmadova630>
2023-05-31 12:31:07 +0400

    last

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from product.views import *
+# app_name='mainapp'
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("list/",product_list_view),
-    path("detail/<int:id>/",product_detail_view),
-    path('create/',product_create_view)
+    path("list/",product_list_view,name='list'),
+    path("detail/<int:id>/",product_detail_view,name='detail'),
+    path('create/',product_create_view,name='create')
 ]
