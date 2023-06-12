bb773ffa50b3209e654d167fc43ed88e262a3c45
leyla ahmadova <leylaahmadova630>
2023-06-12 17:32:59 +0400

    12iyun

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from django.urls import path
 from product.views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("list/",product_list_view,name='list'),
-    path("newlist/",product_newlist_view,name='newlist'),
+    # path("newlist/",product_newlist_view,name='newlist'),
 
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+import os
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,6 +12,28 @@ class Product(models.Model):
     discount_price = models.FloatField( blank = True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
   
     def _str_(self):
         return self.name
+
+
+def upload_to(instance,filename):
+    
+    return f"products/{instance.name.lower().replace(' ','-')}/{filename}"
+def upload_to2(instance,filename):
+    return f"companies/{instance.name.upper().replace(' ','@')}/{filename}"
+
+class ProductImage(models.Model):
+    name=models.CharField(max_length=200)
+    description=RichTextField(blank=True,null=True)
+    price = models.FloatField()
+    image=models.ImageField(upload_to=upload_to)
+    company_image=models.ImageField(upload_to=upload_to2)
+
+    def _str_(self):
+        return self.name
+    
+
+# image yukleneme pathi avtomatik olaraq bele olmalidir --> products/product obyektinin adi ama lowercase olaraq ve aralarda bosluq yerine tire/faylin adi
+# company_logo yuklenme pathi avtomatik olaraq bele olmalidir --> companies/product obyektinin adi ama uppercase olaraq ve aralarda bosluq yerine @/faylin adi
